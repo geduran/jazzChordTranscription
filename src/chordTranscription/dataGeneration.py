38ecc05e92b36ff1213e7ff85cfc055aa23d6cb3
GABRIEL DURAN <geduran@uc.cl>
2020-06-03 01:07:24 -0400

    Data augmentation added

--- src/chordTranscription/dataGeneration.py
+++ src/chordTranscription/dataGeneration.py
@@ -8,17 +8,18 @@ import os
 import numpy    as np
 import pandas   as pd
 
+
 # Own Code Imports
 sys.path.insert(0, '../')
 from transcriptionManager import transcriptionData
 from audioData import AudioData
 
+# from dlUtils import chordEval
 
-
-audio_path = '../../data/JAAH/audios/wav/'
+audio_path = '../../data/JAAH/audios/wav_da/'
 json_path = '../../data/JAAH/data/annotations_/'
 alignments_path = '../../results/segmentSynch/JAAH_results/paths_beats/'
-output_path = '../../data/JAAH/chordTranscription/'
+output_path = '../../data/JAAH/chordTranscription/both_da/'
 
 
 not_useful = ['stompin_at_the_savoy',
@@ -55,11 +56,21 @@ not_useful = ['stompin_at_the_savoy',
               'maple_leaf_rag(hyman)',
               'i_cant_get_started']
 
+test_songs = [
+              'breakfast_feud',
+              'hotter_than_that',
+              'body_and_soul(goodman)',
+              'four_brothers',
+              'minor_swing',
+              'when_lights_are_low'
+             ]
+
+
 hop_len = 2048
 sr = 44100
 method = 'cqt'
 
-
+# CE = chordEval()
 TM = transcriptionData(sr=sr, hop_len=hop_len, workin_dir='',
                        dtw_results_path=alignments_path)
 files = glob.glob(audio_path + '*wav')
@@ -68,23 +79,28 @@ total_time = 0
 single_song_time = 0
 
 for file in files:
-    song_name = file.split('/')[-1].split('.')[0]
+    song_name= file.split('/')[-1].split('.')[0]
+
+    if song_name[-3] == '_':
+        i = int(song_name[-2:])
+        song_name = song_name[:-3]
+    else:
+        i = int(song_name[-1])
+        song_name = song_name[:-2]
 
     if song_name in not_useful:
         continue
 
-    # if 'hotter_than' not in song_name:
-    #     continue
-
-    print('Extracting data from song: ' + song_name)
+    print('   Extracting data from song: ' + song_name +' shifting ' + str(i))
 
     _, audio = AudioData.open_audio(file)
+
     choruses, audio_choruses = TM.get_audio_JAAH_choruses(audio, json_path,
                                                           song_name)
 
     song_info = TM.get_json_info(json_path, song_name)
 
-    cqt_choruses = TM.getCqtChoruses(audio_choruses, song_info, hop_len=hop_len, method=method)
+    cqt_choruses = TM.getCqtChoruses(audio_choruses, info=song_info, hop_len=hop_len, method=method)
 
     dtw_paths = TM.getDtwPaths(song_name)
 
@@ -100,10 +116,39 @@ for file in files:
     assert len(aligned_chords) == warped_cqts[0].shape[1]
     assert len(aligned_chords) == len(beat_labels)
 
-    print('    Has {} sequences and {} frames'.format(len(warped_cqts), len(aligned_chords)))
+    # print('    Has {} sequences and {} frames'.format(len(warped_cqts), len(aligned_chords)))
 
-    coded_roots, coded_notes = TM.codeChordLabels(aligned_chords)
+    coded_roots, coded_notes = TM.codeChordLabels(aligned_chords, transpose=i)
 
+
+
+    #
+    #
+    # from dlUtils import chordEval, encode_labels
+    # CE= chordEval()
+    # name_root = CE.root_name(CE.root_predictions(coded_roots))
+    # name_chord, _ = CE.chord_type_greedy(CE.root_predictions(coded_roots), coded_notes)
+    #
+    # chords = CE.to_chord_label(encode_labels(name_root, name_chord))
+    #
+    # for root, notes, chord in zip(name_root,  name_chord, chords):
+    #     if chord !=  root +':'+ notes:
+    #         print('chord {} -> {}:{}'.format(chord, root, notes))
+    #
+    # continue
+    #
+    #
+    #
+
+
+
+
+
+
+
+
+
+    # print('For i: {}, roots are: {} and notes are: {}'.format(i, coded_roots[:,50:55], coded_notes[:,50:55]))
 
     data = {
             'data': warped_cqts,
@@ -116,30 +161,41 @@ for file in files:
             'method': method
             }
 
-    file_name = song_name + '_' + method + '_' + str(hop_len) + '.pkl'
-    save_path = output_path + method + '/'
+    # gt_roots = CE.root_predictions(coded_roots)
+    # root_name = CE.root_name(gt_roots)
+    # names, types = CE.chord_type_greedy(gt_roots, coded_notes)
+    #
+    # for chord, root, name in zip(aligned_chords, root_name, names):
+    #     if chord != root+':'+name:
+    #         print('chord: {} -> {}:{}'.format(chord, root, name))
+
+    file_name = song_name +str(i)+ '_' + method + '_' + str(hop_len) + '.pkl'
+
+    if song_name in test_songs:
+        save_path = output_path + method + '/test/'
+    else:
+        save_path = output_path + method + '/'
+
 
     out_file = open(save_path + file_name, 'wb')
     pickle.dump(data, out_file)
     out_file.close()
 
 
+    for _,value in choruses.items():
+        if not isinstance(value, dict):
+            continue
+        single_song_time += value['end'] - value['start']
+        break
+
+    for _,value in choruses.items():
+        if not isinstance(value, dict):
+            continue
+        total_time += value['end'] - value['start']
 
 
-    # for _,value in choruses.items():
-    #     if not isinstance(value, dict):
-    #         continue
-    #     single_song_time += value['end'] - value['start']
-    #     break
-    #
-    # for _,value in choruses.items():
-    #     if not isinstance(value, dict):
-    #         continue
-    #     total_time += value['end'] - value['start']
-
-
-# print('total_time: {0:0.2f}'.format(total_time/60))
-# print('single_song_time: {0:0.2f}'.format(single_song_time/60))
+print('total_time: {0:0.2f}'.format(total_time/60))
+print('single_song_time: {0:0.2f}'.format(single_song_time/60))
 
 
 
